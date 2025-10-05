49665245a6a31940238b45a77bfa2e94a0cc45f1
Olaore Fouad <olaorefouad565@gmail.com>
2025-10-05 22:02:47 +0200

    first react project innit

---
+++ first-react-app/src/App.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import "./styles.css";
+
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem",
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering",
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)",
+  ],
+  [
+    "Vanilla JavaScript requires imperative programming",
+    "Imperative Programming: You define all the steps needed to achieve a result",
+    "React on the other hand embraces declarative programming",
+    "With React, you define the goal and React figures out how to get there",
+  ],
+];
+const headers = [
+  "Why React?",
+  "Core Features",
+  "Related Resources",
+  "React vs JS",
+];
+
+export default function App() {
+  const [activeContentIndex, setActiveContentIndex] = useState(0);
+
+  return (
+    <div>
+      <header>
+        <img src="react-logo-xs.png" alt="React logo" />
+        <div>
+          <h1>React.js</h1>
+          <p>i.e., using the React library for rendering the UI</p>
+        </div>
+      </header>
+
+      <div id="tabs">
+        <menu>
+          {headers.map((header, index) => {
+            return (
+              <button
+                className={activeContentIndex === index ? "active" : ""}
+                onClick={() => setActiveContentIndex(index)}
+              >
+                {header}
+              </button>
+            );
+          })}
+        </menu>
+        <div id="tab-content">
+          <ul>
+            {content[activeContentIndex].map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
